e43472b0a90f4e61b995c4409817ddbed70370b7
Felipe Sousa <felipe.fps09@hotmail.com>
2023-05-23 10:11:39 -0300

    Criação dos requests put, post, get e delete e uma busca personalizda na classe ProdutoController

--- src/main/java/com/generation/farmastore/repositories/ProdutoRepository.java
+++ src/main/java/com/generation/farmastore/repositories/ProdutoRepository.java
@@ -2,7 +2,10 @@ package com.generation.farmastore.repositories;
 
 import com.generation.farmastore.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto,Long> {
-
+    public List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
