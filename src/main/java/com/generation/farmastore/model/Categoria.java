4bb7baa55bc9dc14edea888de6140e5abfa96bc8
Felipe Sousa <felipe.fps09@hotmail.com>
2023-05-23 10:37:51 -0300

    Inserindo as validações e relacionamento entre classes Categoria com Produto

--- src/main/java/com/generation/farmastore/model/Categoria.java
+++ src/main/java/com/generation/farmastore/model/Categoria.java
@@ -1,6 +1,10 @@
 package com.generation.farmastore.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tb_categorias")
@@ -9,9 +13,14 @@ public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Campo não pode estar vazio")
     private String tipo;
     private String descricao;
 
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria",  cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("categoria")
+    private List<Produto> produtos;
     public Long getId() {
         return id;
     }
@@ -34,5 +43,9 @@ public class Categoria {
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
     }
 }
